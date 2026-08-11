aa9029b7909b167f43e74284dfa26ba13d502940
Alejandro Villarreal <reyalex316@gmail.com>
2026-08-11 21:55:04 +0400

    task07 storage account name update

--- task07/locals.tf
+++ task07/locals.tf
@@ -1,6 +1,6 @@
 locals {
   rg_name              = format("%s-rg", var.name_prefix)
-  storage_account_name = replace(var.name_prefix, "-", "")
+  storage_account_name = format("%ssa", replace(var.name_prefix, "-", ""))
   fd_profile_name      = format("%s-fd-profile", var.name_prefix)
   fd_endpoint_name     = format("%s-fd-endpoint", var.name_prefix)
   fd_origin_group_name = format("%s-fd-origin-group", var.name_prefix)
