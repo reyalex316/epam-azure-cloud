1ab6be9844233659edede91d10b1d5e1d4808de6
Alejandro Villarreal <reyalex316@gmail.com>
2026-08-29 02:48:21 +0400

    new ip

--- task09/main.tf
+++ task09/main.tf
@@ -23,7 +23,6 @@ module "afw" {
   vnet_id                  = data.azurerm_virtual_network.main.id
   aks_subnet_id            = data.azurerm_subnet.aks.id
   aks_loadbalancer_ip      = var.aks_loadbalancer_ip
-  aks_cluster_name         = var.aks_cluster_name
   fw_subnet_address_prefix = var.fw_subnet_address_prefix
   fw_pip_name              = var.fw_pip_name
   fw_sku_name              = var.fw_sku_name
